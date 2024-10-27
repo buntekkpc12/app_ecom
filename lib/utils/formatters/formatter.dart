eebed217e717d55f9aee8aaa72fcd05f6f0cc0d5
buntek <chhornbuntek@gmail.com>
2024-10-27 18:30:00 +0700

    on pricing

--- lib/utils/formatters/formatter.dart
+++ lib/utils/formatters/formatter.dart
@@ -12,43 +12,41 @@ class TFormatter {
   }
 
   static String formatPhoneNumber(String phoneNumber) {
-    if(phoneNumber.length == 10) {
-        return '(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)} ${phoneNumber.substring(6)}';
+    if (phoneNumber.length == 10) {
+      return '(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
     } else if (phoneNumber.length == 11) {
-      return '(${phoneNumber.substring(0,4)}) ${phoneNumber.substring(4,7)} ${phoneNumber.substring(7)}';
+      return '(${phoneNumber.substring(0, 4)}) ${phoneNumber.substring(4, 7)} ${phoneNumber.substring(7)}';
     }
     return phoneNumber;
   }
 
-  static String internationalPhoneNumber(String phoneNumber) {
-
+  static String internationalFormatPhoneNumber(String phoneNumber) {
     //remove non-digit characters from phone number
     var digitsOnly = phoneNumber.replaceAll(RegExp(r'\D'), '');
 
-  //extract the country code from the digitsOnly
-    String countryCode = '+${digitsOnly.substring(0,2)}';
+    //extract the country code from the digitsOnly
+    String countryCode = '+${digitsOnly.substring(0, 2)}';
     digitsOnly = digitsOnly.substring(2);
 
-  //add the remaining digits with the proper formatting
-  final formattedNumber = StringBuffer();
-  formattedNumber.write('($countryCode)');
+    //add the remaining digits with the proper formatting
+    final formattedNumber = StringBuffer();
+    formattedNumber.write('($countryCode)');
 
-  int i=0;
-  while (i<digitsOnly.length) {
-    int gorupLenght = 2;
-    if (i == 0 && countryCode == '+1') {
-      gorupLenght =3;
+    int i = 0;
+    while (i < digitsOnly.length) {
+      int gorupLenght = 2;
+      if (i == 0 && countryCode == '+1') {
+        gorupLenght = 3;
+      }
+
+      int end = i + gorupLenght;
+      formattedNumber.write(digitsOnly.substring(i, end));
+
+      if (end < digitsOnly.length) {
+        formattedNumber.write(' ');
+      }
+      i = end;
     }
-
-    int end = i + gorupLenght;
-    formattedNumber.write(digitsOnly.substring(i, end));
-
-    if (end < digitsOnly.length) {
-      formattedNumber.write(' ');
-    }
-    i = end;
+    return formattedNumber.toString();
   }
-
-  }
-
 }
