66be0f1ce461d67223d932f8c09535e895cf1e82
buntek <chhornbuntek@gmail.com>
2024-10-30 23:30:17 +0700

    homescreen

---
+++ lib/features/shop/screens/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../common/widgets/custom_shapes/container/circular_container.dart';
+import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimayHeaderContainer(
+              child: Column(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
