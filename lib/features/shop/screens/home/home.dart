3f7b4f44fbb2a0e9af175e6d70a32ebfd06979a3
buntek <chhornbuntek@gmail.com>
2024-11-01 00:12:21 +0700

    home

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,15 +1,11 @@
-import 'package:ecommerce/common/widgets/appbar/appbar.dart';
 import 'package:ecommerce/features/shop/screens/home/widgets/home_appbar.dart';
-import 'package:ecommerce/utils/constants/colors.dart';
-import 'package:ecommerce/utils/constants/text_strings.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
-import '../../../../common/widgets/custom_shapes/container/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/custom_shapes/container/search_container.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,7 +18,34 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimayHeaderContainer(
               child: Column(
-                children: [THomeAppBar()],
+                children: [
+                  THomeAppBar(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  TSearchContainer(
+                    text: 'Search in Store',
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: Colors.white,
+                        ),
+                        SizedBox(
+                          height: TSizes.spaceBtwSections,
+                        ),
+                        THomeCategories(),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             )
           ],
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -14,6 +14,7 @@ class TImages {
   static const String onBoardingImage3 =
       "assets/images/on_boarding_images/dile.gif";
 
+  //animations
   static const String productsIllustration =
       "assets/images/animations/work-out.gif";
   static const String productsSaleIllustration =
@@ -26,4 +27,18 @@ class TImages {
       "assets/images/animations/work-out.gif";
   static const String verifyIllustration =
       "assets/images/animations/work-out.gif";
+
+  //icon logo category
+  static const String sportIcon = "assets/icons/categories/sportIcon.png";
+  static const String clothIcon = "assets/icons/categories/clothIcon.png";
+  static const String shoeIcon = "assets/icons/categories/shoeIcon.png";
+  static const String cosmeticsIcon =
+      "assets/icons/categories/cosmeticsIcon.png";
+  static const String animalIcon = "assets/icons/categories/animalIcon.png";
+  static const String toyIcon = "assets/icons/categories/toyIcon.png";
+  static const String furnitureIcon =
+      "assets/icons/categories/furnitureIcon.png";
+  static const String jeweleryIcon = "assets/icons/categories/jeweleryIcon.png";
+  static const String electronicsIcon =
+      "assets/icons/categories/electronicsIcon.png";
 }
