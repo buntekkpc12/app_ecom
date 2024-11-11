e62841551e20b9e1cdc728155b4ef32b8b7afbcf
buntek <chhornbuntek@gmail.com>
2024-11-11 20:41:34 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   // todo: add widgets binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
   // todo: init local storage
   //todo: await native splash
   //todo: initialize firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //todo: initialize authentication
 
   runApp(const App());
